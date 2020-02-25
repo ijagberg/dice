be08966ac760e52a9679574e339aae167c4b5834
Isak Jägberg <ijagberg@gmail.com>
2020-02-25 19:05:27 +0100

    -

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,9 @@ struct Opts {
     /// Optional aggregate function to apply to the collected rolls of a die.
     ///
     /// One of 'sum', 'avg', 'max', 'min'
-    #[structopt(long)]
+    #[structopt(short, long)]
     aggregate: Option<Aggregate>,
+
     dice_coll: Vec<Dice>,
 }
 
@@ -44,24 +45,34 @@ enum ParseAggregateError {
     InvalidFormat(String),
 }
 
-impl ToString for ParseAggregateError {
-    fn to_string(&self) -> String {
-        format!("")
+impl Display for ParseAggregateError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        Ok(match self {
+            ParseAggregateError::InvalidFormat(message) => {
+                write!(f, "invalid aggregate function: {}", message)?
+            }
+        })
     }
 }
 
 #[derive(Clone, Debug, PartialEq)]
-struct Dice {
+pub struct Dice {
     count: u32,
     sides: u32,
 }
 
 impl Dice {
-    fn new(count: u32, sides: u32) -> Self {
+    pub fn new(count: u32, sides: u32) -> Self {
+        if count == 0 {
+            panic!("count must be greater than 0");
+        }
+        if sides == 0 {
+            panic!("sides must be greater than 0");
+        }
         Self { count, sides }
     }
 
-    fn roll(&self) -> Vec<u32> {
+    pub fn roll(&self) -> Vec<u32> {
         let mut rng = rand::thread_rng();
         (0..self.count)
             .map(|_| rng.gen_range(1, self.sides + 1))
@@ -72,37 +83,6 @@ impl Dice {
 impl Display for Dice {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}d{}", self.count, self.sides)
-    }
-}
-
-fn main() {
-    let opts = Opts::from_args();
-
-    for d in opts.dice_coll {
-        let rolls = d.roll();
-        println!(
-            "{} {}",
-            d,
-            match opts.aggregate {
-                None => format!("{:?}", rolls),
-                Some(Aggregate::Sum) => format!("{}", rolls.iter().sum::<u32>()),
-                Some(Aggregate::Avg) => format!("{}", rolls.iter().sum::<u32>() / d.count),
-                Some(Aggregate::Max) => format!(
-                    "{}",
-                    rolls
-                        .iter()
-                        .max()
-                        .expect("called aggregate max on empty iter")
-                ),
-                Some(Aggregate::Min) => format!(
-                    "{}",
-                    rolls
-                        .iter()
-                        .min()
-                        .expect("called aggregate min on empty iter")
-                ),
-            }
-        );
     }
 }
 
@@ -146,15 +126,53 @@ impl FromStr for Dice {
 }
 
 #[derive(Clone, Debug, PartialEq)]
-enum ParseDieError {
+pub enum ParseDieError {
     InvalidFormat(String),
 }
 
-impl ToString for ParseDieError {
-    fn to_string(&self) -> String {
-        match self {
-            ParseDieError::InvalidFormat(s) => format!("parsing die failed with message: {}", s),
-        }
+impl Display for ParseDieError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        Ok(match self {
+            ParseDieError::InvalidFormat(message) => {
+                write!(f, "parsing die failed with message: {}", message)?
+            }
+        })
+    }
+}
+
+fn main() {
+    let opts = Opts::from_args();
+
+    if opts.dice_coll.is_empty() {
+        eprintln!("Provide some dice to roll")
+    }
+
+    for d in opts.dice_coll {
+        let rolls = d.roll();
+        println!(
+            "{} {}",
+            d,
+            match opts.aggregate {
+                None => format!("{:?}", rolls),
+                Some(Aggregate::Sum) => format!("{}", rolls.iter().sum::<u32>()),
+                Some(Aggregate::Avg) =>
+                    format!("{}", rolls.iter().sum::<u32>() as f32 / d.count as f32),
+                Some(Aggregate::Max) => format!(
+                    "{}",
+                    rolls
+                        .iter()
+                        .max()
+                        .expect("called aggregate max on empty iter")
+                ),
+                Some(Aggregate::Min) => format!(
+                    "{}",
+                    rolls
+                        .iter()
+                        .min()
+                        .expect("called aggregate min on empty iter")
+                ),
+            }
+        );
     }
 }
 
@@ -164,8 +182,45 @@ mod tests {
 
     #[test]
     fn parse() {
-        let expected = Dice { count: 6, sides: 5 };
-
+        let expected = Dice::new(6, 5);
         assert_eq!(expected, Dice::from_str("6d5").unwrap());
+
+        let expected = Dice::new(1, 120);
+        assert_eq!(expected, Dice::from_str("d120").unwrap());
+
+        assert!(Dice::from_str("-5d20").is_err());
+        assert!(Dice::from_str("5d-20").is_err());
     }
+
+    #[test]
+    fn roll() {
+        for count in 1..=100 {
+            for sides in 1..=100 {
+                let dice = Dice::new(count, sides);
+                let rolls = dice.roll();
+                for roll in rolls {
+                    assert!(roll >= 1 && roll <= sides);
+                }
+            }
+        }
+    }
+
+    // #[test]
+    // fn distribution() {
+    //     use std::collections::HashMap;
+
+    //     let mut map = HashMap::new();
+    //     let dice = Dice::new(1, 100);
+    //     for _ in 0..100_000 {
+    //         let rolls = dice.roll();
+    //         for roll in rolls {
+    //             let counter = map.entry(roll).or_insert(0);
+    //             *counter += 1;
+    //         }
+    //     }
+
+    //     for key in 1..=100 {
+    //         println!("{}: {}", key, map[&key]);
+    //     }
+    // }
 }
